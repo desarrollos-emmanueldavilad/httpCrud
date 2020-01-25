52b0b9fe6f6c5e4404f1edc6dcb60c845df31028
chadwick <desarrollos.emmanueldavilad@gmail.com>
2020-01-26 00:28:19 +0100

    code

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HeroeComponent } from './components/heroes/heroe.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'heroe/:id', component: HeroeComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'heroes'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
